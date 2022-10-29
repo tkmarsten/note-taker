89e7e130f81c3e9fd31b9a5fb977112bd9739268
tkmarsten <tkmarsten@gmail.com>
2022-10-28 21:29:21 -0700

    Add ID to notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const { readFromFile, readAndAppend } = require('../utils/fsUtil')
+const { readFromFile, readAndAppend, writeToFile } = require('../utils/fsUtil')
+var uniqid = require('uniqid')
 
 router.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
@@ -9,10 +10,21 @@ router.post('/api/notes', (req, res) => {
   const { title, text } = req.body
 
   if (req.body) {
-    const newNote = { title, text }
+    const newNote = {
+      title,
+      text,
+      id: uniqid()
+    }
 
     readAndAppend(newNote, './db/db.json')
   }
 })
+
+// router.delete('/api/notes/:id', (req, res) => {
+//   let filtered = readFromFile('./db/db.json')
+//     .then((data) => res.json(JSON.parse(data.filter(note => note.id !== req.params.id))))
+
+//   writeToFile('./db/db.json', filtered)
+// })
 
 module.exports = router
